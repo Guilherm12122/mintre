721d80310978eae952d0d3a4b32f9880407a22eb
Guilherme de Oliveira Ribeiro <guilherme.oliveira.ribeiro1@ibm.com>
2024-10-16 08:11:50 -0300

    criação de navbar

--- mintre_front/src/app/app.module.ts
+++ mintre_front/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { RouterOutlet } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterOutlet,
+    MatButtonModule
   ],
   providers: [
     provideClientHydration(),
